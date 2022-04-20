32dc4ded1b96affca9c8028a3e5242e5710cca6f
mohaddeseht <103627084+mohaddeseht@users.noreply.github.com>
2022-04-20 23:28:46 +0430

    Update q3.js

--- q3.js
+++ q3.js
@@ -1,10 +1,11 @@
 let str = 'the quick brown fox';
 function upperCase(str) {
-    let arr = str.split('')
+    let arr = str.split(' ')
     let newArr = [];
-    for (let x in arr) {
-        newArr.push(arr[x].charAt(0).toUpperCase() + arr[x].slice(1));
+    for (let item of arr) {
+        newArr.push(item[0].toUpperCase() + item.slice(1));
     }
-    return newArr.join('');
+    return newArr.join(' ');
 }
-console.log(upperCase(str));
+let result = upperCase(str);
+console.log(result);
